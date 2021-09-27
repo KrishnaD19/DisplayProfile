06ea548505128b8dd2d389aa1e9ad944611068bb
amulyagit <nittala.s@northeastern.edu>
2021-09-26 23:45:18 -0400

    first commit - Amulya
    first commit - Amulya

---
+++ src/model/Product.java
@@ -0,0 +1,181 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.awt.Image;
+
+/**
+ *
+ * @author Amulya
+ */
+public class Product {
+    
+    private String Name;
+    private String Gdata;
+    private String DOB;
+    private long Tnumber;
+    private long Fnumber;
+    private String Email;
+    private long SSN;
+    private long Medical;
+    private long Health;
+    private long Bank;
+    private String Vehicle;
+    private String License;
+    private String Device;
+    private String Linkedin;
+    private String IP;
+    private String image;
+    private String Unique;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getGdata() {
+        return Gdata;
+    }
+
+    public void setGdata(String Gdata) {
+        this.Gdata = Gdata;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public void setFnumber(int Fnumber) {
+        this.Fnumber = Fnumber;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    
+    }
+
+    public String getLinkedin() {
+        return Linkedin;
+    }
+
+    public void setLinkedin(String Linkedin) {
+        this.Linkedin = Linkedin;
+    }
+
+    public String getIP() {
+        return IP;
+    }
+
+    public void setIP(String IP) {
+        this.IP = IP;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    
+
+
+    public String getUnique() {
+        return Unique;
+    }
+
+    public void setUnique(String Unique) {
+        this.Unique = Unique;
+    }
+
+    public String getVehicle() {
+        return Vehicle;
+    }
+
+    public void setVehicle(String Vehicle) {
+        this.Vehicle = Vehicle;
+    }
+
+    public String getLicense() {
+        return License;
+    }
+
+    public void setLicense(String License) {
+        this.License = License;
+    }
+
+    public String getDevice() {
+        return Device;
+    }
+
+    public void setDevice(String Device) {
+        this.Device = Device;
+    }
+
+    public long getTnumber() {
+        return Tnumber;
+    }
+
+    public void setTnumber(long Tnumber) {
+        this.Tnumber = Tnumber;
+    }
+
+    public long getFnumber() {
+        return Fnumber;
+    }
+
+    public void setFnumber(long Fnumber) {
+        this.Fnumber = Fnumber;
+    }
+
+    public long getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(long SSN) {
+        this.SSN = SSN;
+    }
+
+    public long getMedical() {
+        return Medical;
+    }
+
+    public void setMedical(long Medical) {
+        this.Medical = Medical;
+    }
+
+    public long getHealth() {
+        return Health;
+    }
+
+    public void setHealth(long Health) {
+        this.Health = Health;
+    }
+
+    public long getBank() {
+        return Bank;
+    }
+
+    public void setBank(long Bank) {
+        this.Bank = Bank;
+    }
+    
+}
+
+
+
